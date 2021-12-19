b2fc62c279e6bbc6be863dae03a9d09282177664
cbboss404 <phongphu1042@gmail.com>
2021-12-19 23:25:24 +0700

    build UserController_method_userregister && updaterPass_user

--- controller/UserController.php
+++ controller/UserController.php
@@ -17,7 +17,13 @@ function alertM($smg, $link)
 $user_group_action = $_POST["user_group_action"];
 switch ($user_group_action) {
             case 'user_create':
-
+                $txt_name = $_POST["txt_name"];
+                $txt_email = $_POST["txt_email"];
+                $txt_password = md5($_POST["txt_password"]);
+                $txt_phone = $_POST["txt_phone"];
+                $txt_Address = $_POST["txt_Address"];
+                UserDao::insertUser($txt_name, $txt_email, $txt_password, $txt_phone, $txt_Address);
+                alertM("Đăng Ký Thành Công", "../view/login.php");
                 break;
             case 'user_login':
 
@@ -47,8 +53,26 @@ switch ($user_group_action) {
                 }
                 break;
             case 'user_update':
+                $login_email = $_POST["login_email"];
+                $login_password = md5($_POST["login_password"]);
+                $new_password = md5($_POST["new_password"]);
+                $data = UserDao::getUser($login_email);
 
-          
+
+                if (isset($data["user_email"]) == false) {
+                    alertM("Tên đăng nhập này không tồn tại. Vui lòng kiểm tra lại. !!!", "../view/updatePassUser.php");
+                    exit;
+                } elseif (!$login_email || !$login_password) {
+                    alertM("Vui lòng nhập đầy đủ tên đăng nhập và mật khẩu !!! ", "../view/updatePassUser.php");
+                    exit;
+                } elseif ($login_password != $data["user_pass"]) {
+                    alertM("Mật khẩu bạn không chính xác. Vui lòng kiểm tra lại. !!!", "../view/updatePassUser.php");
+                    exit;
+                } elseif ($login_email == $data["user_email"] && $login_password == $data["user_pass"]) {
+                    UserDao::updateUser($login_email, $new_password);
+                    alertM("Cập Nhật Thành Công Thành Công !!! ", "../view/login.php");
+                    exit;
+                }
                 break;
         }
     
---
+++ view/updatePassUser.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <?php include 'layout/headerpage.php'; ?>
+</head>
+
+<body>
+    <header>
+        <!-- Header Start -->
+        <?php include 'layout/header1page.php'; ?>
+        <!-- Header End -->
+    </header>
+    <main>
+        <!-- Hero Area Start-->
+        <div class="slider-area ">
+            <div class="single-slider slider-height2 d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="hero-cap text-center">
+                                <h2>Update User</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero Area End-->
+        <!--================login_part Area =================-->
+        <section class="login_part section_padding ">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-6 col-md-6">
+                        <div class="login_part_text text-center">
+                            <div class="login_part_text_iner">
+                                <h2>New to our Shop?</h2>
+                                <p>There are advances being made in science and technology
+                                    everyday, and a good example of this is the</p>
+                                <a href="register.php" class="btn_3">Create an Account</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-6 col-md-6">
+                        <div class="login_part_form">
+                            <div class="login_part_form_iner">
+                                <h3>Welcome Back ! <br>
+                                    Please Sign in now</h3>
+                                <form class="row contact_form" action="../controller/UserController.php" method="POST" novalidate="novalidate">
+                                    <div class="col-md-12 form-group p_star">
+                                        <input type="email" class="form-control" id="login_email" name="login_email" value="" placeholder="Email">
+                                    </div>
+                                    <div class="col-md-12 form-group p_star">
+                                        <input type="password" class="form-control" id="login_password" name="login_password" value="" placeholder="Password">
+                                    </div>
+                                    <div class="col-md-12 form-group p_star">
+                                        <input type="password" class="form-control" id="new_password" name="new_password" value="" placeholder="New-Password">
+                                    </div>
+                                    <div class="col-md-12 form-group">
+
+
+                                        <button type="submit" id="btn_test" name="user_group_action" value="user_update" class="btn_3">
+                                            Update
+                                        </button>
+
+                                        <a class="lost_pass" href="#">forget password?</a>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!--================login_part end =================-->
+    </main>
+    <footer>
+        <!-- Footer Start-->
+        <?php include 'layout/footerpage.php'; ?>
+        <!-- Footer End-->
+    </footer>
+    <!--? Search model Begin -->
+    <?php include 'layout/search.php'; ?>
+    <!-- Search model end -->
+
+    <!-- JS here -->
+
+    <?php include 'layout/scriptpage.php'; ?>
+
+</body>
+
+</html>
