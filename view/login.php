bffd3d24c954f9ffea773ebc5f9e6dae599acc94
cbboss404 <phongphu1042@gmail.com>
2021-12-19 23:03:37 +0700

    first layout user login

---
+++ view/login.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="zxx">
+
+<head>
+    <?php include 'layout/headerpage.php'; ?>
+</head>
+
+<body>
+    <header>
+        <!-- Header Start -->
+        <?php include 'layout/header1page.php'; ?>
+        <!-- Header End -->
+    </header>
+    <main>
+        <!-- Hero Area Start-->
+        <div class="slider-area ">
+            <div class="single-slider slider-height2 d-flex align-items-center">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-xl-12">
+                            <div class="hero-cap text-center">
+                                <h2>Login</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Hero Area End-->
+        <!--================login_part Area =================-->
+
+        <!--================login_part end =================-->
+    </main>
+    <footer>
+        <!-- Footer Start-->
+        <?php include 'layout/footerpage.php'; ?>
+        <!-- Footer End-->
+    </footer>
+    <!--? Search model Begin -->
+    <?php include 'layout/search.php'; ?>
+    <!-- Search model end -->
+
+    <!-- JS here -->
+
+    <?php include 'layout/scriptpage.php'; ?>
+
+</body>
+
+</html>
