e07dc3e8a3ed8993c34897721d18ead4dbc27a49
cbboss404 <phongphu1042@gmail.com>
2021-12-20 11:05:10 +0700

    load database ListUser

--- admin/view/UserList.php
+++ admin/view/UserList.php
@@ -71,6 +71,49 @@
                           </tr>
                       </thead>
                       <tbody>
+                        <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname="doan";
+                        try {
+                            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                            $stmt = $conn->prepare("SELECT `user_id`, `user_name`, `user_email`, `user_pass`, `user_phone`, `user_address` FROM `user`");
+                            $stmt->execute();
+                            $user= $stmt-> fetchALL(PDO::FETCH_ASSOC);
+                            IF($user){
+                                foreach($user as $user){
+                                  echo'<form action="../controller/usersController.php" method="POST" enctype="multipart/form-data" class="form-horizontal"';
+                                    echo '<tr>';
+                                    echo '<td class = "avatar">';
+                                    echo '<div class = "round-img">';
+                                    echo '<a href = "#"><img class = "rounded-circle" src = "images/1.jpg" alt = ""></a>';
+                                    echo '</div>';
+                                    echo '</td>';
+                                    echo '<td name="txt_id">'.$user['user_id'].'</td>';
+                                    echo '<td name="txt_name"> <span class = "name">'.$user['user_name'].'</span> </td>';
+                                    echo '<td name="txt_email"> <span class = "price">'.$user['user_email'].'</span> </td>';
+                                    echo '<td name="txt_phone">';
+                                    echo '<span class = "type phone">'.$user['user_phone'].'</span>';
+                                    echo '</td>';
+                                    echo '<td name="txt_adress"> <span class = "adress">'.$user['user_address'].'</span> </td>';
+                                    echo '<td width="50px">';
+                                    echo '<a href="userseditpage.php?edit='. $user['user_id'] .'" type = "button" class = "btn btn-success"><i class = "fa fa-edit"></i> Sửa</a>';
+                                    echo '</td>';
+                                    echo '<td width="50px">';
+                                    echo '<a href="../controller/usersController.php?delete='. $user['user_id'] .'" type = "button" class = "btn btn-danger" "  ><i class = "fa fa-times"></i> Xóa</a>';
+                                    echo '</td>';
+                                    echo '</tr>';
+                                    echo '</form>';
+                                }
+                            }
+                        }
+                        catch(PDOException $e) {
+                          echo "Error: " . $e->getMessage();
+                            }
+                            $conn = null;
+                        ?>
 
                       </tbody>
                     </table>
