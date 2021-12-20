46f099d460a68e334b988896ea65e81e41700248
cbboss404 <phongphu1042@gmail.com>
2021-12-20 12:10:22 +0700

    base_Controller

--- admin/view/orderList.php
+++ admin/view/orderList.php
@@ -71,7 +71,47 @@
                       </thead>
                       <tbody>
                       <tbody>
-                      
+                       <?php
+                        $servername = "localhost";
+                        $username = "root";
+                        $password = "";
+                        $dbname="demo2";
+                        try {
+                            $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                            $stmt = $conn->prepare("SELECT `order_id`, `user_id`, `order_date`, `order_status`, `amount` FROM `order`");
+                            $stmt->execute();
+                            $product= $stmt-> fetchALL(PDO::FETCH_ASSOC);
+                            IF($product){
+                                foreach($product as $product){
+                                  echo'<form action="../controller/productController.php" method="POST" enctype="multipart/form-data" class="form-horizontal"';
+                                    echo '<tr>';
+                                    
+                                    echo '<td name="txt_oder_id">'.$product['order_id'].'</td>';
+                                    echo '<td name="txt_user_id"> <span class = "user_id">'.$product['user_id'].'</span> </td>';
+                                    echo '<td name="txt_date"> <span class = "date">'.$product['order_date'].'</span> </td>';
+                                    echo '<td name="txt_status">';
+                                    echo '<span class = "type status">'.$product['order_status'].'</span>';
+                                    echo '</td>';
+                                    echo '<td name="txt_amount"> <span class = "amount">'.$product['amount'].'</span> </td>';
+                                    echo '<td width="50px">';
+                                    echo '<a href="producteditpage.php?edit='. $product['order_id'] .'" type = "button" class = "btn btn-success"><i class = "fa fa-edit"></i> Sửa</a>';
+                                    echo '</td>';
+                                    echo '<td width="50px">';
+                                    echo '<a href="../controller/productController.php?delete='. $product['order_id'] .'" type = "button" class = "btn btn-danger" "  ><i class = "fa fa-times"></i> Xóa</a>';
+                                    echo '</td>';
+                                    echo '</tr>';
+                                    echo '</form>';
+                                }
+                            }
+                        }
+                        catch(PDOException $e) {
+                          echo "Error: " . $e->getMessage();
+                            }
+                            $conn = null;
+                        ?>
+
+                       
                       </tbody>
 
                       </tbody>
