6c45c10304686c68c6ae08aa79838f23663e6080
cbboss404 <phongphu1042@gmail.com>
2021-12-20 11:19:31 +0700

    create layout edit/adđ_User

---
+++ admin/view/userseditpage.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <?php include 'layout/header.php'; ?>
+
+</head>
+
+<body>
+
+  <!--*******************
+        Preloader start
+    ********************-->
+  <?php include 'layout/Preloader_start.php'; ?>
+  <!--*******************
+        Preloader end
+    ********************-->
+
+
+  <!--**********************************
+        Main wrapper start
+    ***********************************-->
+  <div id="main-wrapper">
+
+    <!--**********************************
+            Nav header start
+        ***********************************-->
+     <?php include 'layout/Nav_header_start.php'; ?>
+    <!--**********************************
+            Nav header end
+        ***********************************-->
+
+    <!--**********************************
+            Header start
+        ***********************************-->
+     <?php include 'layout/Header_start.php'; ?>
+    <!--**********************************
+            Header end ti-comment-alt
+        ***********************************-->
+
+    <!--**********************************
+            Sidebar start
+        ***********************************-->
+     <?php include 'layout/Sidebar_start.php'; ?>
+    <!--**********************************
+            Sidebar end
+        ***********************************-->
+
+    <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-lg-12">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="form-validation">
+                                  
+
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+
+
+        <!--**********************************
+            Footer start
+        ***********************************-->
+       <?php include 'layout/Footer.php'; ?>
+    <!--**********************************
+            Footer end
+        ***********************************-->
+ 
+  <!--**********************************
+        Main wrapper end
+    ***********************************-->
+
+  <!--**********************************
+        Scripts
+    ***********************************-->
+  <?php include 'layout/Scripts.php'; ?>
+
+</body>
+
+</html>
