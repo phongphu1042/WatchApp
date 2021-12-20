2ab0dc5657e1d102714828349e59738fb108a8b5
cbboss404 <phongphu1042@gmail.com>
2021-12-20 12:11:56 +0700

    create add order

--- admin/view/orderListCreate.php
+++ admin/view/orderListCreate.php
@@ -56,7 +56,37 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="form-validation">
-                                    
+                                    <form action="productcontroller.php" method="POST" enctype="multipart/form-data" class="form-horizontal">
+                                        <div class="row form-group">
+                                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">id đơn hàng</label></div>
+                                            <div class="col-12 col-md-9"><input type="text" id="txt_iddh" name="txt_iddh" class="form-control"></div>
+                                        </div>
+                                        <div class="row form-group">
+                                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">id khách hàng</label></div>
+                                            <div class="col-12 col-md-9"><input type="text" id="txt_idkh" name="txt_idkh" class="form-control"></div>
+                                        </div>
+                                        <div class="row form-group">
+                                            <div class="col col-md-3"><label for="text-input" class=" form-control-label">ngày</label></div>
+                                            <div class="col-12 col-md-9"><input type="text" id="txt_ngay" name="txt_ngay" class="form-control"></div>
+                                        </div>
+                                        <div class="row form-group">
+                                            <div class="col col-md-3"><label for="email-input" class=" form-control-label">price</label></div>
+                                            <div class="col-12 col-md-9"><input id="txt_price" name="txt_price" class="form-control" require></div>
+                                        </div>
+                                        <div class="row form-group">
+                                            <div class="col col-md-3"><label for="password-input" class=" form-control-label">status</label></div>
+                                            <div class="col-12 col-md-9"><input  id="txt_status" name="txt_status" class="form-control"></div>
+                                        </div>
+                                       
+                                        <div>
+                                            <button name="product_group_action" value="product_create" type="submit" class="btn btn-primary btn-sm">
+                                                <i class="fa fa-dot-circle-o"></i> Create
+                                            </button>
+                                            <button name="btn_reset" type="reset" class="btn btn-danger btn-sm">
+                                                <i class="fa fa-ban"></i> Reset
+                                            </button>
+                                        </div>
+                                    </form>
                                 </div>
                             </div>
                         </div>
